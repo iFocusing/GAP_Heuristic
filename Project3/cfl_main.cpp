d198f7c6cf6199edb3b7f5b0e25f9b6463f5d535
Jingjing huo <jingjing.huo@rwth-aachen.de>
2018-10-26 05:52:08 -0700

    Print average facility opening cost, capacity and customer service cost to verify that the instance is correctly created, and solved by our model.

--- Project3/cfl_main.cpp
+++ Project3/cfl_main.cpp
@@ -24,5 +24,27 @@ Computational Optimization in Logistics: Tutorial 1
 		if (quit) return 0;
 		cflsol sol(cfl);
 
+		std::cout << "Capacitated Facility Location Problem Name:" << cfl.probname << std::endl;
+		std::cout << "The number of facilities:" << cfl.nPlants << std::endl;
+		std::cout << "The number of customers:" << cfl.nCustom << std::endl; 
+
+		double SumOpenCosts = 0;
+		double SumPlantCapa = 0;
+		double SumCustCosts = 0;
+
+		for (int j = 0; j < cfl.nPlants; j++) {
+			SumOpenCosts = SumOpenCosts + cfl.OpenCosts[j];
+			SumPlantCapa = SumPlantCapa + cfl.PlantCapa[j];
+			for (int i = 0; i < cfl.nCustom; i++){
+				SumCustCosts += cfl.CustCosts[i][j];
+			}
+		}
+
+		std::cout << "Average open cost:" << SumOpenCosts / cfl.nPlants << std::endl;
+		std::cout << "Average plant capacity:" << SumPlantCapa / cfl.nPlants << std::endl;
+		std::cout << "Average custome cost:" << SumCustCosts / cfl.nCustom << std::endl;
+
+		std::cout << "Press enter to quit." << std::endl;
+		getchar();
 
 	}
